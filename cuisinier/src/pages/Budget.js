2cac6d65427b7a174d3e361579a470d42ac04ace
Craig Neeson <craigneeson@hotmail.co.uk>
2022-05-28 13:32:15 +0100

    dietary requirements

--- cuisinier/src/pages/Budget.js
+++ cuisinier/src/pages/Budget.js
@@ -2,6 +2,7 @@ import { Heading } from "../components/Heading";
 import { SubHeading } from "../components/SubHeading";
 import { SteppedInput } from "../components/SteppedInput";
 import { BudgetSlider } from "../components/BudgetSlider";
+import { DietaryRequirementItem } from "../components/DietaryRequirementItem";
 import { useStepper } from '../hooks/useStepper';
 import { useBudgetSlider } from '../hooks/useBudgetSlider';
 
@@ -22,6 +23,14 @@ function Budget() {
                 onStepUp={onStepUp}
                 onStepDown={onStepDown}
             />
+
+            <>
+                <DietaryRequirementItem label="No requirements" subLabel="All recipes are suitable" />
+                <DietaryRequirementItem label="Vegetarian" subLabel="Only vegetable-based" />
+                <DietaryRequirementItem label="Vegan" subLabel="Only plant-based" />
+                <DietaryRequirementItem label="Pescaterian" subLabel="Only seafood" />
+                <DietaryRequirementItem label="Keto" subLabel="Low carbohydrate" />
+            </>
         </>
     );
 }
