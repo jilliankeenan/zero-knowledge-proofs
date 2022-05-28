f0432698858a555fe8e1d0dc1c6ab91d09b33e82
Jillian Keenan <jilliankeenan@Jillians-MacBook-Pro-2.local>
2022-05-28 11:39:16 +0100

    PUSHING TO MAIN YOLO

--- cuisinier/src/App.js
+++ cuisinier/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Budget from './pages/Budget'
+import Profile from './pages/Profile'
+import Grocers from './pages/Grocers'
+import Splash from './pages/Splash'
+import Complete from './pages/Complete'
+import Recipe from './pages/Recipe'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/complete" element={<Complete />}/>
+        <Route path="/recipe" element={<Recipe />}/>
+        <Route path="/grocers" element={<Grocers />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/budget" element={<Budget />}/>
+        <Route path="/splash" element={<Splash />}/>
+        <Route path="/" element={<Complete />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
